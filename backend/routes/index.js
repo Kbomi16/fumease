cf49c261510a37a3a71ff21dfa2c6f32eea214c9
YDA0 <dayoung9746@gmail.com>
2023-10-12 11:16:00 +0900

    MySQL 연결 및 OpenAPI test 완료

---
+++ backend/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get("/list", async function (req, res) {
+  var list = await Perfume.findAll()
+  res.json(list)
+})
+router.get("/recommand", async function (req, res) {
+  var list = await Perfume.findAll()
+  let keywordList = []
+  list.forEach(perfume => {
+    keywordList.push({ name: perfume.f_name, scent: perfume.f_scent })
+  })
+  res.json(keywordList)
+})
+
+module.exports = router;
