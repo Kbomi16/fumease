110f751dcb6d4c640dfa1079b437caf414be22aa
김보미 <kimbomi2172@naver.com>
2023-11-16 17:43:09 +0900

    keyword test하기

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -63,5 +63,56 @@ router.get("/detail/:f_id", async function (req, res) {
   }
 });
 
+// GPT
+const OpenAI= require('openai');
+router.get("/chatgpt",async function(req,res){
+  try {
+    const product = await Perfume.findOne({
+      attributes: ['f_id', 'f_name', 'f_price', 'f_scent', 'f_note', 'f_img', 'f_brand', 'f_keyword']
+    });
+    res.json(product);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).send('Error fetching products');
+  }
+  const { selectedKeywords } = req.query;
+
+  const openai = new OpenAI({
+    apiKey: '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', // defaults to process.env["OPENAI_API_KEY"]
+  });
+  const thread = await openai.beta.threads.create();
+  const message = await openai.beta.threads.messages.create(
+    thread.id,
+    {
+      role: "user",
+      content: selectedKeywords
+    }
+  );
+
+  const run = await openai.beta.threads.runs.create(
+    thread.id,
+    { 
+      assistant_id: "asst_y798sFICs2u5xBo4RPOG7SIg",
+      // instructions: "사용자가 키워드를 입력하면 해당 키워드에 어울리는 향수를 추천해주는 시스템. 향수데이터는 향수정보.pdf 를 참고해서 추천해줘.향수 이름,향수가격,브랜드가 출력되게해줘."
+    }
+  );
+  var intervalPromise=setInterval(async ()=>{
+    const check = await openai.beta.threads.runs.retrieve(
+      thread.id,
+      run.id
+    );
+    console.log(check)
+    if(check.status=="completed"){
+      clearInterval(intervalPromise)
+      const messages = await openai.beta.threads.messages.list(
+        thread.id
+      );
+      
+    }
+    
+  },3000)
+  
+  // console.log(messages.data[])
+})
 
 module.exports = router;
--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,98 +1,186 @@
 // Home.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Home.module.css'; // styles를 import
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css"; // styles를 import
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
   // 키워드 가져오기
   const [keywords, setKeywords] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/keyword', {
-      headers: {
-        'Access-Control-Allow-Origin': '*', // 허용할 Origin
-      },
-    })
+    axios
+      .get("http://localhost:3001/keyword", {
+        headers: {
+          "Access-Control-Allow-Origin": "*", // 허용할 Origin
+        },
+      })
       .then((response) => {
         const keywordArray = response.data;
         // 데이터가 배열인지 확인
         if (Array.isArray(keywordArray)) {
           // 모든 키워드를 쪼개어 각 단어로 분할
           const allKeywords = keywordArray.reduce((acc, keyword) => {
-            const keywords = keyword.f_keyword.split(',').map(keyword => keyword.trim());
+            const keywords = keyword.f_keyword
+              .split(",")
+              .map((keyword) => keyword.trim());
             return [...acc, ...keywords];
           }, []);
-  
-          // 중복 제거를 위해 Set 사용
-        const uniqueKeywords = Array.from(new Set(allKeywords));
-        setKeywords(uniqueKeywords);
 
-        
+          // 중복 제거를 위해 Set 사용
+          const uniqueKeywords = Array.from(new Set(allKeywords));
+          setKeywords(uniqueKeywords);
         } else {
-          console.error('Received invalid data', keywordArray);
+          console.error("Received invalid data", keywordArray);
         }
       })
       .catch((error) => {
-        console.error('Error fetching keywords:', error);
+        console.error("Error fetching keywords:", error);
       });
   }, []);
 
-  //선택한 키워드로 향수 추천해주기
+  // 알림 메시지를 위한 상태 추가
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const handleKeywordClick = (keyword) => {
+    // 이미 선택된 키워드인지 확인
+    if (selectedKeywords.includes(keyword)) {
+      setAlertMessage("이미 선택된 키워드입니다.");
+      return;
+    }
+
+    // 선택된 키워드가 3개인지 확인
+    if (selectedKeywords.length === 3) {
+      setAlertMessage("이미 3개의 키워드를 선택하셨습니다.");
+      return;
+    }
+
+    setSelectedKeywords([...selectedKeywords, keyword]);
+    setAlertMessage("키워드를 선택하세요.");
+  };
+  // 사용자가 선택한 키워드를 저장하는 state
   const [selectedKeywords, setSelectedKeywords] = useState([]);
 
-  //사용자가 키워드를 선택할 때 호출된다.
-  const handleKeywordSelection = (keyword) => {
-    if (selectedKeywords.length < 3) {
-      setSelectedKeywords((prevKeywords) => [...prevKeywords, keyword]);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  // 사용자가 선택한 향수 추천 결과를 저장하는 상태
+  const [recommendedPerfumes, setRecommendedPerfumes] = useState([]);
+
+  // GPT-4에게 향수 추천을 요청하는 함수
+  const handleRecommendationClick = async () => {
+    if (selectedKeywords.length === 3) {
+      try {
+        // selectedKeywords를 쉼표로 구분된 문자열로 변환
+        const keywordString = selectedKeywords.join(",");
+        const response = await axios.get(
+          `http://localhost:3001/chatgpt?selectedKeywords=${keywordString}`
+        );
+        const recommendedPerfumes = response.data;
+        setRecommendedPerfumes(recommendedPerfumes); // 추천 결과 저장
+        handleShowModal(); // 모달 창 표시
+      } catch (error) {
+        console.error("향수 가져오지 못함");
+      }
+    } else {
+      console.log("키워드 3개를 선택하세요.");
     }
   };
 
-
+  const PerfumeList = ({ recommendedPerfumes }) => {
+    return (
+      <div>
+        {recommendedPerfumes.map((perfume, index) => (
+          <div key={index}>
+            <h3>{perfume.name}</h3>
+            <p>{perfume.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   return (
-      <div className={styles['main-page']}>
-        <div className={styles.main}>
-          <div className={styles['left-section']}>
-            <img className={styles.mainimg} src="img/main1.png" alt="Left Section" />
-          </div>
-
-          <div className={styles['right-section']}>
-            <img className={styles.mainimg} src="https://web-resource.tamburins.com/assets/image/main/toilet_fragrance/pc_mainHero_right.jpg" alt="Right Section" />
-          </div>
+    <div className={styles["main-page"]}>
+      <div className={styles.main}>
+        <div className={styles["left-section"]}>
+          <img
+            className={styles.mainimg}
+            src="img/main1.png"
+            alt="Left Section"
+          />
         </div>
-        <button className={styles.gotoScent}>
-          <Link to="/scent" style={{ textDecoration: 'none', color: '#000' }}>
-            제품 보기
-          </Link>
+
+        <div className={styles["right-section"]}>
+          <img
+            className={styles.mainimg}
+            src="https://web-resource.tamburins.com/assets/image/main/toilet_fragrance/pc_mainHero_right.jpg"
+            alt="Right Section"
+          />
+        </div>
+      </div>
+      <button className={styles.gotoScent}>
+        <Link to="/scent" style={{ textDecoration: "none", color: "#000" }}>
+          제품 보기
+        </Link>
+      </button>
+
+      <div className={styles.ai}>
+        <h1>TODAY PERFUME</h1>
+        <p>오늘의 날씨와 기분에 따라 원하는 키워드 3가지를 선택해보세요.</p>
+        <div className={styles["keyword-buttons"]}>
+          {keywords.map((keyword, index) => {
+            // 이전에 keyword가 #으로 시작하는지 확인
+            const isHexColor = keyword.startsWith("#");
+            // 버튼 스타일을 동적으로 설정
+            const buttonStyle = isHexColor ? { backgroundColor: keyword } : {};
+
+            // hex값이 검정색인 경우 글자 색을 흰색으로 설정
+            if (isHexColor && keyword === "#000000") {
+              buttonStyle.color = "white";
+            }
+
+            // 선택된 키워드인 경우 배경색과 글자색 변경
+            if (selectedKeywords.includes(keyword)) {
+              buttonStyle.backgroundColor = "black";
+              buttonStyle.color = "white";
+            }
+            return (
+              <button
+                key={index}
+                id={keyword}
+                className={styles.keyword}
+                style={buttonStyle}
+                onClick={() => handleKeywordClick(keyword)}
+              >
+                {keyword}
+              </button>
+            );
+          })}
+          {/* 알림 메시지 출력 */}
+          <p className={styles.p1}>{alertMessage}</p>
+        </div>
+        <button className={styles.aiBtn} onClick={handleRecommendationClick}>
+          추천 향수 보기
         </button>
 
-        <div className={styles.ai}>
-          <h1>TODAY PERFUME</h1>
-          <p>오늘의 날씨와 기분에 따라 원하는 키워드 3가지를 선택해보세요.</p>
-          <div className={styles['keyword-buttons']}>
-          {keywords.map((keyword, index) => {
-  // 이전에 keyword가 #으로 시작하는지 확인
-  const isHexColor = keyword.startsWith('#');
-  // 버튼 스타일을 동적으로 설정
-  const buttonStyle = isHexColor ? { backgroundColor: keyword } : {};
-
-  // hex값이 검정색인 경우 글자 색을 흰색으로 설정
-  if (isHexColor && keyword === "#000000") {
-    buttonStyle.color = 'white';
-  }
-
-  return (
-    <button key={index} className={styles.keyword} style={buttonStyle}>
-      {keyword}
-    </button>
-  );
-})}
+        {showModal && (
+          <div className={styles.modal}>
+            <h1>추천 향수</h1>
+            <PerfumeList recommendedPerfumes={recommendedPerfumes} />
+            <button onClick={handleCloseModal}>닫기</button>
+          </div>
+        )}
       </div>
-      <button className={styles.aiBtn}>추천 향수 보기</button>
-      </div>
-        </div>
+    </div>
   );
 };
 
