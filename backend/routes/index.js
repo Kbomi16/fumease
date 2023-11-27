39981cd782564a65dcb8870d84b4d43db12a0680
김보미 <kimbomi2172@naver.com>
2023-11-27 16:47:15 +0900

    모달-추천향수 띄우기

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -42,17 +42,6 @@ router.get("/keyword", async function (req, res) {
     res.status(500).send("Error fetching products");
   }
 });
-// router.get("/detail", async function (req, res) {
-//   try {
-//     const products = await Perfume.findAll({
-//       attributes: ['f_id', 'f_name', 'f_price', 'f_scent', 'f_note', 'f_img', 'f_brand']
-//     });
-//     res.json(products);
-//   } catch (error) {
-//     console.error('Error fetching products:', error);
-//     res.status(500).send('Error fetching products');
-//   }
-// })
 
 router.get("/detail/:f_id", async function (req, res) {
   try {
@@ -79,77 +68,108 @@ router.get("/detail/:f_id", async function (req, res) {
 const OpenAI = require("openai");
 
 router.get("/chatgpt", async function (req, res) {
-  // 사용자가 선택한 키워드들을 요청 쿼리에서 가져온다.
-  const { selectedKeywords } = req.query;
-  const openai = new OpenAI({
-    apiKey: "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", // defaults to process.env["OPENAI_API_KEY"]
-  });
+  try {
+    // 사용자가 선택한 키워드들을 요청 쿼리에서 가져온다.
+    const { selectedKeywords } = req.query;
+    const openai = new OpenAI({
+      apiKey: "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",
+    });
 
-  // 스레드를 통해 사용자와의 대화를 관리.
-  const thread = await openai.beta.threads.create();
-  const message = await openai.beta.threads.messages.create(thread.id, {
-    role: "user",
-    content: selectedKeywords, // 메시지의 내용은 사용자가 선택한 키워드가 들어감.
-  });
+    // 스레드를 통해 사용자와의 대화를 관리.
+    const thread = await openai.beta.threads.create();
+    const message = await openai.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: selectedKeywords,
+    });
 
-  const run = await openai.beta.threads.runs.create(thread.id, {
-    assistant_id: "asst_y798sFICs2u5xBo4RPOG7SIg",
-    instructions:
-      "사용자가 키워드를 입력하면 해당 키워드에 어울리는 향수를 추천해주는 시스템. 향수데이터는 향수정보.pdf 를 참고해서 추천해줘.향수 이름,향수가격,브랜드가 출력되게해줘.",
-  });
+    console.log("create run");
+    const run = await openai.beta.threads.runs.create(thread.id, {
+      assistant_id: "asst_y798sFICs2u5xBo4RPOG7SIg",
+    });
+    console.log("create run", run);
 
-  //  3초마다 GPT 모델의 실행 상태를 확인하고, 실행이 완료되면 결과를 가져와서 클라이언트에게 반환하는 작업을 수행함
-  var intervalPromise = setInterval(async () => {
-    const check = await openai.beta.threads.runs.retrieve(thread.id, run.id);
-    console.log(check);
+    let check;
+    do {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      check = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+    } while (check.status !== "completed");
 
-    if (check.status == "completed") {
-      clearInterval(intervalPromise);
-      const messages = await openai.beta.threads.messages.list(thread.id);
-      console.log("Messages:", messages);
-      try {
-        // OpenAI에서 받은 응답을 줄 단위로 분리
-        let content = messages.data[0].content;
-        const perfumeNames = content
-          .map((line) => {
-            // line.text.value 접근
-            const index = line.text.value.indexOf("**향수 이름**:");
-            if (index !== -1) {
-              return line.text.value
-                .substring(index + "**향수 이름**:".length)
-                .trim();
+    const messages = await openai.beta.threads.messages.list(thread.id);
+    console.log("Messages:", messages);
+    console.log("First message content:", messages.data[0].content[0]);
+
+    // GPT답변 텍스트를 줄 단위로 분리하고, 각 행을 배열로 반환함.
+    function textToArray(text) {
+      const lines = text.split("\n");
+      const perfumes = [];
+      let currentPerfume = {};
+
+      lines.forEach((line, index) => {
+        const [key, value] = line.split(" : ");
+
+        if (key !== undefined && value !== undefined) {
+          if (key.includes("향수 이름")) {
+            if (Object.keys(currentPerfume).length !== 0) {
+              if (currentPerfume["brand"]) {
+                currentPerfume["brand"] = currentPerfume["brand"]
+                  .replace(/\[.*?\]/g, "")
+                  .trim();
+              }
+              perfumes.push(currentPerfume);
+              currentPerfume = {};
             }
-            return null;
-          })
-          .filter((name) => name != null); // null과 undefined 제거
+            currentPerfume["name"] = value.trim();
+          } else if (key.includes("향수 가격")) {
+            currentPerfume["price"] = value.trim();
+          } else if (key.includes("브랜드")) {
+            currentPerfume["brand"] = value;
+          }
+        }
 
-        // 추출한 각 향수 이름에 대해 데이터베이스에서 정보를 찾아 반환
-        const perfumes = await Promise.all(
-          perfumeNames.map((perfumeName) => {
-            return Perfume.findOne({
-              attributes: [
-                "f_id",
-                "f_name",
-                "f_price",
-                "f_scent",
-                "f_note",
-                "f_img",
-                "f_brand",
-                "f_keyword",
-              ],
-              where: {
-                f_name: perfumeName,
-              },
-            });
-          })
-        );
-        res.json(perfumes.filter((perfume) => perfume != null)); // null과 undefined 제거
-      } catch (error) {
-        console.error("Error fetching perfumes:", error);
-        res.status(500).send("Error fetching perfumes");
-      }
+        if (index === lines.length - 1 || index + 1 === lines.length) {
+          if (currentPerfume["brand"]) {
+            currentPerfume["brand"] = currentPerfume["brand"]
+              .replace(/\[.*?\]/g, "")
+              .trim();
+          }
+          perfumes.push(currentPerfume);
+        }
+      });
+      return perfumes;
     }
-  }, 3000);
+    console.log("First message content:", messages.data[0].content[0]);
+
+    // OpenAI에서 받은 응답을 처리
+    const content = messages.data[0].content[0].text.value;
+    const list = textToArray(content);
+    // 수정된 부분: GPT 응답에서 추출한 향수 이름들
+    const perfumeNames = list.map((perfume) => perfume.name);
+
+    // 이름을 기반으로 데이터베이스에서 향수 상세 정보 가져오기
+    const perfumes = await Promise.all(
+      perfumeNames.map((perfumeName) => {
+        if (perfumeName) {
+          return Perfume.findOne({
+            attributes: [
+              "f_id",
+              "f_name",
+              "f_price",
+              "f_img",
+              "f_brand",
+              "f_keyword",
+            ],
+            where: {
+              f_name: perfumeName,
+            },
+          });
+        }
+      })
+    );
+    res.json(perfumes.filter((perfume) => perfume != null));
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).send("Error occurred during GPT processing");
+  }
 });
 
 module.exports = router;
--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,9 +1,12 @@
 // Home.js
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import styles from "./Home.module.css"; // styles를 import
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import Modal from 'react-modal';
+
 
 const Home = () => {
   // 키워드 가져오기
@@ -46,8 +49,11 @@ const Home = () => {
   const handleKeywordClick = (keyword) => {
     // 이미 선택된 키워드인지 확인
     if (selectedKeywords.includes(keyword)) {
-      setAlertMessage("이미 선택된 키워드입니다.");
-      return;
+      // 이미 선택된 키워드를 취소하는 부분
+    const updatedKeywords = selectedKeywords.filter((key) => key !== keyword);
+    setSelectedKeywords(updatedKeywords);
+    setAlertMessage(`${keyword} 키워드가 취소되었습니다.`);
+    return;
     }
 
     // 선택된 키워드가 3개인지 확인
@@ -62,22 +68,16 @@ const Home = () => {
   // 사용자가 선택한 키워드를 저장하는 state
   const [selectedKeywords, setSelectedKeywords] = useState([]);
 
-  const [showModal, setShowModal] = useState(false);
-
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
   // 사용자가 선택한 향수 추천 결과를 저장하는 상태
   const [recommendedPerfumes, setRecommendedPerfumes] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false); // 로딩 상태를 관리하는 state
 
   // GPT-4에게 향수 추천을 요청하는 함수
   const handleRecommendationClick = async () => {
     if (selectedKeywords.length === 3) {
+      setIsLoading(true); // 데이터를 가져오기 전에 로딩 상태를 true로 설정
+      setIsModalOpen(true);
       try {
         // selectedKeywords를 쉼표로 구분된 문자열로 변환
         const keywordString = selectedKeywords.join(",");
@@ -86,25 +86,83 @@ const Home = () => {
         );
         const recommendedPerfumes = response.data;
         setRecommendedPerfumes(recommendedPerfumes); // 추천 결과 저장
+        setIsModalOpen(true);
+        console.log("handleRecommendationClick called")
       } catch (error) {
         console.error("향수 가져오지 못함");
       }
+      setIsLoading(false); // 응답을 받은 후 로딩 상태를 false로 설정
+      
     } else {
       console.log("키워드 3개를 선택하세요.");
     }
   };
 
+  const handleReset = () => {
+    setIsModalOpen(false); // 모달을 닫습니다.
+    setSelectedKeywords([]); // 선택된 키워드를 초기화합니다.
+    setRecommendedPerfumes([]); // 추천된 향수 목록을 초기화합니다.
+  };
+
   const PerfumeList = ({ recommendedPerfumes }) => {
     return (
-      <div>
+      <Modal
+  className="perfumeresult"
+  isOpen={isModalOpen}
+  onRequestClose={closeModal}
+  style={{
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      zIndex: 1000,
+    },
+    content: {
+      position: 'relative',
+      top: '5%',
+      left: '10%',
+      width: '80%',
+      height: '90%',
+      borderRadius: '10px',
+      backgroundColor: 'rgba(255, 255, 255, 0.9)',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: 'none'
+    },
+  }}
+>
+        <Row xs={2} md={4} lg={4} className={styles.row}> 
         {recommendedPerfumes.map((perfume, index) => (
-          <div key={index}>
-            <h3>{perfume.name}</h3>
-            <p>{perfume.description}</p>
-          </div>
-        ))}
-      </div>
+        <Col key={index} className={styles.mb4}> {/* 향수 카드를 매핑하는 컴포넌트에 Col 적용 */}
+          <Card>
+            <Card.Img variant="top" src={perfume.f_img}/>
+            <Card.Body>
+              <Card.Title className={styles['title']}>{perfume.f_name}</Card.Title>
+              <Card.Text className={styles['text']}>{perfume.f_price}원</Card.Text>
+              <Card.Text className={styles['text']}>{perfume.f_brand}</Card.Text>
+              <Button variant="primary" className={styles.btn} onClick={() => Navigate(`/${perfume.f_id}`)}>MORE</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+      </Row>
+      {/* 다시하기 버튼을 맨 아래 한 번만 나타나도록 위치 조정 */}
+      {recommendedPerfumes.length > 0 && (
+          <Button className={styles.aiBtn} onClick={handleReset}>다시 하기</Button>
+      )}
+        
+      </Modal>
     );
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -134,45 +192,59 @@ const Home = () => {
 
       <div className={styles.ai}>
         <h1>TODAY PERFUME</h1>
-        <p>오늘의 날씨와 기분에 따라 원하는 키워드 3가지를 선택해보세요.</p>
-        <div className={styles["keyword-buttons"]}>
-          {keywords.map((keyword, index) => {
-            // 이전에 keyword가 #으로 시작하는지 확인
-            const isHexColor = keyword.startsWith("#");
-            // 버튼 스타일을 동적으로 설정
-            const buttonStyle = isHexColor ? { backgroundColor: keyword } : {};
 
-            // hex값이 검정색인 경우 글자 색을 흰색으로 설정
-            if (isHexColor && keyword === "#000000") {
-              buttonStyle.color = "white";
-            }
+        {isLoading ? (
+          <p>퓨미즈 AI가 선택 키워드를 기반으로 향수를 추천 중이에요! 조금만 기다려주세요...</p>
+        ) : (
+          <div>
+            <p>오늘의 날씨와 기분에 따라 원하는 키워드 3가지를 선택해보세요.</p>
+            <div className={styles["keyword-buttons"]}>
+              {keywords.map((keyword, index) => {
+                // 이전에 keyword가 #으로 시작하는지 확인
+                const isHexColor = keyword.startsWith("#");
+                // 버튼 스타일을 동적으로 설정
+                const buttonStyle = isHexColor
+                  ? { backgroundColor: keyword }
+                  : {};
 
-            // 선택된 키워드인 경우 배경색과 글자색 변경
-            if (selectedKeywords.includes(keyword)) {
-              buttonStyle.backgroundColor = "black";
-              buttonStyle.color = "white";
-            }
-            return (
-              <button
-                key={index}
-                id={keyword}
-                className={styles.keyword}
-                style={buttonStyle}
-                onClick={() => handleKeywordClick(keyword)}
-              >
-                {keyword}
-              </button>
-            );
-          })}
-          {/* 알림 메시지 출력 */}
-          <p className={styles.p1}>{alertMessage}</p>
-        </div>
-        <button className={styles.aiBtn} onClick={handleRecommendationClick}>
-          추천 향수 보기
-        </button>
+                // hex값이 검정색인 경우 글자 색을 흰색으로 설정
+                if (isHexColor && keyword === "#000000") {
+                  buttonStyle.color = "white";
+                }
 
-        {recommendedPerfumes.length > 0 && (
-          <PerfumeList recommendedPerfumes={recommendedPerfumes} />
+                // 선택된 키워드인 경우 배경색과 글자색 변경
+                if (selectedKeywords.includes(keyword)) {
+                  buttonStyle.backgroundColor = "black";
+                  buttonStyle.color = "white";
+                }
+                return (
+                  <button
+                    key={index}
+                    id={keyword}
+                    className={styles.keyword}
+                    style={buttonStyle}
+                    onClick={() => handleKeywordClick(keyword)}
+                  >
+                    {keyword}
+                  </button>
+                );
+              })}
+              {/* 알림 메시지 출력 */}
+              <p className={styles.p1}>{alertMessage}</p>
+            </div>
+
+            <button
+              className={styles.aiBtn}
+              onClick={handleRecommendationClick}
+            >
+              추천 향수 보기
+            </button>
+            {recommendedPerfumes && (
+              <div>
+                <PerfumeList recommendedPerfumes={recommendedPerfumes} />
+              </div>
+            )}
+          </div>
         )}
       </div>
     </div>
